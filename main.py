f43a33f3c55f0dea8b30ac11f44672945f0053e5
Olixierd <oliwier9440023@gmail.com>
2026-09-01 10:08:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+Python
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Le bot est en ligne !'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+keep_alive()
 import discord
 from discord.ext import commands, tasks
 from discord import app_commands
